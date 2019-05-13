222c0aed3d13928adafb18835307c828088087a9
davisharlan <davis.s.harlan@gmail.com>
2019-05-12 21:39:59 -0500

    Remove unecessary condition in isMoveValid

--- src/main/scala/validation/MoveValidation.scala
+++ src/main/scala/validation/MoveValidation.scala
@@ -2,14 +2,10 @@ package validation
 
 import utils.MoveUtils.applyMoveToState
 import components.{Move, State}
-import utils.BuildUtils
 
 object MoveValidation {
   def isMoveValid(state: State, move: Move): Boolean =
     StateValidation.isStateValid(applyMoveToState(state, move)) &&
       state.board(move.end._1)(move.end._2) - state.board(move.start._1)(
-        move.start._2) < 1 &&
-      BuildUtils
-        .getAllPotentialBuilds(applyMoveToState(state, move), move)
-        .length > 0
+        move.start._2) < 1
 }
