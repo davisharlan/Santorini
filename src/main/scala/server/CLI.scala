a7b9c8bb5b1bd25dfeb3c965d4d65ceec2725742
charles <charlesalberthunt@gmail.com>
2019-05-13 19:28:05 -0500

    REST api and dependencies with mock and ping endpoint

---
+++ src/main/scala/server/CLI.scala
@@ -0,0 +1,11 @@
+package server
+
+class CLI {
+
+  def main(args: Array[String]): Unit = {
+
+    println("Welcome to the Santorini CLI!")
+
+  }
+
+}
---
+++ src/main/scala/server/HTTPServer.scala
@@ -0,0 +1,34 @@
+package server
+
+import cats.effect._
+import cats.implicits._
+import org.http4s.HttpRoutes
+import org.http4s.syntax._
+import org.http4s.dsl.io._
+import org.http4s.implicits._
+import org.http4s.server.blaze._
+
+object HTTPServer extends IOApp {
+
+  val httpService = HttpRoutes.of[IO] {
+    case GET -> Root / "api" / "board" / boardId =>
+      //val boardState = getBoardState(boardId)
+      //val jsonBoardState = boardState.toJson
+      Ok() //<--- put the json board state here
+    case POST -> Root / "api" / "board" / boardId / "player" / playerId / "piece" / pieceId / "move" / location =>
+      //val isValid = validateMode(....)
+      //val newState = applyMove(boardId, playerId, pieceId, location).toJson
+      Ok() //<--- put the new board state here
+    case GET -> Root / "ping" =>
+      Ok("Pong!")
+  }.orNotFound
+
+  def run(args: List[String]): IO[ExitCode] =
+    BlazeServerBuilder[IO]
+      .bindHttp(8081, "localhost")
+      .withHttpApp(httpService)
+      .serve
+      .compile
+      .drain
+      .as(ExitCode.Success)
+}
